66eeccbd0638bbdc8ee72edab2d648a8f94a00c0
James Mullet <86495895+jamesm224@users.noreply.github.com>
2023-11-25 12:09:08 -0500

    Update co_localization_experiment.sh

--- bin/co_localization_experiment.sh
+++ bin/co_localization_experiment.sh
@@ -4,13 +4,12 @@
 #SBATCH -A prudenlab
 #SBATCH -N 1 
 #SBATCH -n 24
-#module load Anaconda
-source activate BiopythonProject
-#module purge
 
-
+##### Set Working Directory #####
 TESTDIR='/projects/ciwars/jamesm22/filter_experiments'
+metadata='mobileOG-db-beatrix-1.6-All.csv'
 
+##### Define Filtering Parameters used in Optimization Experiment #####
 plasmid_purities='50 60 70 80 90'
 phage_purities='50 60 70 80 90'
 filter_phages='50 60 70 80'
@@ -18,6 +17,7 @@ filter_plasmids='50 60 70 80'
 min_plasmids='10 15 20 25 30 35'
 min_phages='10 15 20 25 30 35'
 
+##### Process every possible experimental value #####
 cd ${TESTDIR}
 for plasmid_purity in $plasmid_purities
 do
@@ -31,9 +31,9 @@ for min_plasmid in $min_plasmids
 do
 for min_phage in $min_phages
 do
-metadata='mobileOG-db-beatrix-1.6-All.csv'
 
-python ${TESTDIR}/Hybrid1_optimization.py --o ${plasmid_purity}:${phage_purity}:${filter_phage}:${filter_plasmid}:${min_plasmid}:${min_phage} --i ${metadata} --p ${plasmid_purity} --h ${phage_purity} --fp ${filter_plasmid} --fh ${filter_phage} --mp ${min_plasmid} --mh ${min_plasmid}
+##### Run Co-localization script #####
+python ${TESTDIR}/co_localization_experiment.py --o ${plasmid_purity}:${phage_purity}:${filter_phage}:${filter_plasmid}:${min_plasmid}:${min_phage} --i ${metadata} --p ${plasmid_purity} --h ${phage_purity} --fp ${filter_plasmid} --fh ${filter_phage} --mp ${min_plasmid} --mh ${min_plasmid}
 
 done
 done
