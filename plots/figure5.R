477c77972b13c5e1791a9753782ba16174f6319d
James Mullet <86495895+jamesm224@users.noreply.github.com>
2023-12-02 11:00:40 -0500

    Create figure5.R

---
+++ plots/figure5.R
@@ -0,0 +1,134 @@
+##### Load Packages and Input Files #####
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(RColorBrewer)
+library(cowplot)
+library(viridis)
+library(ggpubr)
+library(rstatix)
+library(tidyverse)
+library(Cairo)
+
+####Source Location Frequency Graph ####
+source_meta <- read.csv(file = 'arg_total_counts.csv')
+
+##### Order the variables for the graph #####
+source_meta$Source_location <- factor(source_meta$Source_location, 
+                                      levels = c("Host-associated", "Unclassified","Aquatic", "Terrestrial"))
+source_meta
+
+##### Graph the distribution of ARGs by Location #####
+source_location_summary <- ggplot(source_meta, aes(fill=Source_location, y=counts,x=Sample)) + 
+  geom_bar(position="dodge", stat="identity",color='black')+
+  theme_classic()+
+  theme(axis.text.x=element_blank(),
+        legend.text = element_text(family = "Times New Roman",size = 12),
+        legend.title = element_text(family = "Times New Roman",size = 12),
+        axis.title.x=element_text(size=15,color='Black'),
+        axis.text.y=element_text(hjust=1,size=15,color='Black'),
+        legend.position='top',
+        text = element_text(family="Times New Roman",size=15,color = "Black"),)+
+  ylab('Number of ARGs')+
+  xlab('')+
+  guides(fill=guide_legend(title="Source Location"))+
+  # Custom Colors used in graph
+  scale_fill_manual(breaks=c('Aquatic', 'Host-associated', 'Terrestrial','Unclassified'), 
+                    values = alpha(c("#a3cef1",
+                                     "#ff6166",
+                                     "#80ed99",
+                                     "#D3D3D3")))+
+  scale_y_continuous(limits = c(0,125),expand = c(0, 0))+
+  scale_x_discrete(expand = c(0, 0.5))
+source_location_summary
+
+####Bar Chart of ARG Gene Distribution ####
+
+##### Load Dataframe #####
+arg_data <- read.csv(file = 'Hybrid_ARG_Source_Out.csv')
+arg_data
+
+##### Remove genome columns #####
+arg_subset<- subset(arg_data, select = -c(Genome))
+arg_subset
+
+##### Obtain the total counts of ARGs per source location #####
+arg_counts <- arg_subset %>% group_by(ARG,Home_location,Specific.Location,Major_class) %>% 
+  summarise(total_count=n(),.groups = 'drop') %>%
+  as.data.frame()
+arg_counts 
+
+##### Obtain all possible combinations of ARGs and source locations (equal zero values later)
+all_args <- arg_counts %>% expand(ARG, Specific.Location,Major_class) %>%
+  as.data.frame() #%>% 
+#filter(!grepl('Other', Specific.Location))
+all_args %>% dplyr::anti_join(arg_counts)%>%
+  as.data.frame()
+all_args
+
+
+arg_merge<- arg_counts %>% dplyr::right_join(all_args)%>%
+  as.data.frame()
+arg_merge[is.na(arg_merge)] = 0
+arg_merge
+
+##### Filter out the rare ARGs #####
+high_count=arg_merge %>% group_by(ARG) %>% 
+  summarise(total_count=sum(total_count)) %>%
+  filter(total_count>= 12) %>%
+  as.data.frame()
+high_count
+
+##### Calculate Genome Counts by location #####
+genome_count<- subset(arg_data, select = -c(ARG)) %>% 
+  group_by(Home_location,Specific.Location) %>% 
+  summarise(total_count=n()) %>%
+  as.data.frame()
+genome_count
+
+##### Calculate Frequency of ARGs from each sample #####
+merged_out <- arg_merge %>% inner_join(genome_count, 
+                                 by=c('Home_location','Specific.Location'))
+
+merged_out$freq <- merged_out$total_count.x / merged_out$total_count.y *100
+merged_out
+
+
+##### Plot ARG counts by Source Location #####
+bar1 <- merged_out%>%filter(ARG%in%high_count$ARG) %>%
+  ggplot(aes(fill=Specific.Location,
+             y=freq, x=reorder(ARG, total_count.y))) +
+  geom_bar(position="stack", stat="identity",color='black')+
+  theme_classic()+
+  ylab('proportion of AMR genes (%)')+
+  scale_fill_brewer(palette = "Set2")+
+  facet_grid(rows=vars(Major_class),scales="free",space="free")+
+  theme(text = element_text(family="Times New Roman",size=15,color = "black"),
+    axis.text.x = element_text(color='black',size=15),
+    axis.title.y=element_blank(),
+    axis.text.y = element_text(color='black',size=15),
+    legend.text = element_text(family = "Times New Roman",size = 12),
+    legend.title = element_text(family = "Times New Roman",size = 12),
+    legend.position = "top",
+    legend.justification = c("right"),
+    panel.border=element_blank(),
+    strip.text.y = element_text(size = 14,angle=0))+
+  guides(fill=guide_legend(title="Source Location"))+
+  scale_y_continuous(limits = c(0,60), expand = c(0, 0))
+
+bar1
+
+##### Flip Axis #####
+bar2 <- bar1 +coord_flip()
+bar2
+
+##### Combine the Two Additional Plots #####
+##### Figure 5C was created on Proksee #####
+figure_5 <- plot_grid(source_location_summary,bar2,
+                   labels = c("A","B"),rel_widths = c(2/5,3/5),align = "h", axis = "bt")
+figure_5
+
+##### Save and Export Figure #####
+Cairo(file = "figure5.svg", type = "svg", width = 100, height = 50, units = "in", dpi = 400)
+print(figure_5)  # 'final_graph' is the ggplot object
+dev.off()
