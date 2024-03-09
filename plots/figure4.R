ad0244eed845eced1e3938e858fd9bd5ee8a63af
James Mullet <86495895+jamesm224@users.noreply.github.com>
2024-03-09 08:57:06 -0500

    Update figure4.R

--- plots/figure4.R
+++ plots/figure4.R
@@ -14,6 +14,7 @@ library(tidyverse)
 library(pheatmap)
 library(reshape2)
 library(scales) 
+library(dunn.test)
 library(Cairo)
 
 ##### Load Input Data #####
@@ -23,14 +24,18 @@ data
 ##### Obtain Counts of Accessory Genes #####
 accessory_count<- data %>% 
   group_by(Home_location,Specific.Location,Accessory_gene_type,gene_id) %>% 
-  summarise(total_count=n()) %>%
+  summarise(total_count=n(),.groups = "drop") %>%
+  #filter(total_count >= 2) %>%
+  #filter(Accessory_gene_type == 'metabolism')%>% 
   as.data.frame()
 accessory_count
 
 ##### Obtain the number of Genomes #####
 genome_count<- data %>% 
   group_by(Home_location,Specific.Location) %>% 
-  summarise(total_count=n()) %>%
+  summarise(total_count=n(),.groups = "drop") %>%
+  #filter(total_count >= 2) %>%
+  #filter(Accessory_gene_type != 'defense_system')%>% 
   as.data.frame() 
 
 ##### Keys for Plotting #####
@@ -47,6 +52,7 @@ count_key <- data %>%
   group_by(gene_id) %>% 
   summarise(total_count=n()) %>%
   filter(total_count >= 25) %>%
+  #filter(Accessory_gene_type != 'defense_system')%>% 
   as.data.frame() 
 count_key
 
@@ -56,7 +62,7 @@ expanded_data
 complete_data <- expanded_data %>%
   full_join(accessory_count, by = c("Specific.Location", "gene_id"))
 complete_data
-complete_data <- complete_data %>% replace(is.na(.), 0.00001)
+complete_data <- complete_data %>% replace(is.na(.), 0.00000001)
 complete_data
 #complete_data <- cbind(complete_data,name_small_key)
 
@@ -73,16 +79,27 @@ merged_df_1 <- subset(merged_df_1, select = -c(Accessory_gene_type.x,Accessory_g
 merged_df_1
 
 ##### Factor Variables to reorder Graph axis #####
-merged_df_2 <- merged_df_1 %>% inner_join(genome_count, by=c('Home_location','Specific.Location'))
+merged_df_2 <- merged_df_1 %>% inner_join(genome_count, 
+                                          by=c('Home_location','Specific.Location'))
+
 merged_df_2$freq <- merged_df_2$total_count.x / merged_df_2$total_count.y *100
 merged_df_2$Specific.Location <- factor(merged_df_2$Specific.Location, 
                                         levels = c("freshwater", "saltwater", "wastewater",
                                                    "fungi", "plant","animal","human",
                                                    "soil", "sediment", "human facilities","other",
                                                    "unclassified P-P","plasmid","phage"))
+
 merged_df_2$Home_location <- factor(merged_df_2$Home_location, 
                                     levels = c("Aquatic","Host-associated", "Terrestrial", "Other"))
 merged_df_2
+
+row_total <- merged_df_2 %>% 
+  count(`Specific.Location`, wt = n)
+
+genome_count <- merged_df_2 %>%
+  group_by(Specific.Location) %>%
+  summarise(Sum = sum(total_count.x))
+genome_count
 
 ##### Graph the Heatmap #####
 ggheatmap <- merged_df_2%>%filter(gene_id%in%count_key$gene_id) %>%
@@ -91,80 +108,24 @@ ggheatmap <- merged_df_2%>%filter(gene_id%in%count_key$gene_id) %>%
   coord_flip()+
   theme_classic()+
   facet_grid(rows=vars(Accessory_gene_type),cols=vars(Home_location),scales="free",space="free",switch="both")+
-  theme(axis.text.x = element_text(family = "Times New Roman",color='black',angle=90,hjust=1,size=16),
-        axis.text.y = element_text(family = "Times New Roman",color='black',size=16),
+  theme(axis.text.x = element_text(family = "Helvetica",color='black',angle=90,hjust=1,size=16,vjust=0.5),
+        axis.text.y = element_text(family = "Helvetica",color='black',size=16),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
-        legend.text = element_text(family = "Times New Roman",size = 16),
-        legend.title = element_text(family = "Times New Roman",size = 16),
-        strip.text = element_text(family = "Times New Roman", size = 16),
+        legend.text = element_text(family = "Helvetica",size = 16),
+        legend.title = element_text(family = "Helvetica",size = 16),
+        strip.text = element_text(family = "Helvetica", size = 16),
         strip.background = element_blank())+
   guides(fill=guide_legend(title="Log Frequency of Accessory Genes"))
 ggheatmap
 
-
-##### Adjust Colors and Formating for Plot #####
-redone_graph <- ggheatmap +
+##### Cleaned Color Heatmap ####
+cleaned_graph <- ggheatmap +
   scale_fill_gradientn(
     trans = 'log10',
     colors = c("grey90", "#F0F8FF","#B0E0E6" ,"#56B4E9", "#0072B2","#001F3F"),
-    #limits = c(0.01, max(merged_df_2$freq)),  # Start limits from a small positive value close to zero
     breaks = c(0, 0.01, 1, 10, 100, 1000),
-    labels = c("No Data", "0.01", "1", "10", "100", "1000"),
-    guide = guide_colorbar(title = "Your Legend Title Including Zero", barwidth = 10, barheight = 1))
+    labels = c("No Data", "0.01", "1", "10", "100", "1000")
+  )
 
-redone_graph
-
-##### Save and Export Figure #####
-Cairo(file = "figure4.svg", type = "svg", width = 100, height = 100, units = "in", dpi = 500)
-print(redone_graph)  # 'final_graph' is the ggplot object
-dev.off()
-
-##### Statistical Analysis #####
-
-### Obtain the Mean P-P counts per accessory gene ###
-phage_plasmid_subset<- cleaned_stats_data %>% 
-  filter(Specific.Location != 'phage')%>% 
-  filter(Specific.Location != 'plasmid')%>%
-  group_by(gene_id) %>% 
-  # Obtain the Mean Count #
-  summarise(total_count=mean(total_count.x)) %>%
-  as.data.frame()
-phage_plasmid_subset$MGE <- 'P-P'
-phage_plasmid_subset
-
-### Obtain the Phage counts per accessory gene ###
-phage_subset<- merged_df_2 %>% 
-  filter(Specific.Location == 'phage')%>% 
-  group_by(gene_id) %>% 
-  # Obtain the Mean Count #
-  summarise(total_count=mean(total_count.x)) %>%
-  as.data.frame()
-phage_subset$MGE <- 'phage'
-phage_subset
-
-### Obtain the Phage counts per accessory gene ###
-plasmid_subset<- merged_df_2 %>% 
-  filter(Specific.Location == 'plasmid')%>% 
-  group_by(gene_id) %>% 
-  # Obtain the Mean Count #
-  summarise(total_count=mean(total_count.x)) %>%
-  as.data.frame()
-plasmid_subset$MGE <- 'plasmid'
-plasmid_subset
-
-### Combine the phage, plasmid, and P-P dataframes ###
-combined_mge_df <- rbind(phage_plasmid_subset, phage_subset, plasmid_subset)
-combined_mge_df
-
-### Kruskal-Wallis test ##
-kruskal_result <- kruskal.test(total_count ~ MGE, data = combined_mge_df)
-
-### Dunn correction for comparing multiple variables ###
-dunn_result <- dunn.test(combined_mge_df$total_count, g = combined_mge_df$MGE, method = "bonferroni")
-
-# Print Kruskal-Wallis outputs #
-print(kruskal_result)
-
-# Print Dunn correction outputs #
-print(dunn_result)
+cleaned_graph
