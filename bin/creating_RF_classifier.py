7480fa94c1c7e59d4755b9c5d35cfe11619c149f
James Mullet <86495895+jamesm224@users.noreply.github.com>
2024-03-09 15:25:44 -0500

    Update creating_RF_classifier.py

--- bin/creating_RF_classifier.py
+++ bin/creating_RF_classifier.py
@@ -5,12 +5,14 @@ os.chdir('/projects/ciwars/jamesm22/TestData_Hybrids/')
 path = "."
 dir_list = os.listdir(path)
 
+##### Load input files and convert MGE classes to integers #####
 workingtable=pd.read_csv("RF_testing_training_data.csv")
 workingtable= workingtable.replace('plasmid','0', regex=True)
 workingtable= workingtable.replace('phage','1', regex=True)
 workingtable= workingtable.replace('Hybrid','2', regex=True)
 workingtable=workingtable.drop(columns=['Specific Contig','Unnamed: 0'])
 
+##### Obtain test and training data #####
 Test=workingtable.sample(frac=0.2,random_state=57)
 #Test=workingtable.sample(frac=0.3)
 Training=workingtable[~workingtable.isin(Test)].dropna()
@@ -19,7 +21,7 @@ X1 = Training.drop("MGE Element Type", axis=1)
 X=np.array(X1)
 y=np.array(Training['MGE Element Type'])
 
-
+##### Run Classifier #####
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 
@@ -27,3 +29,5 @@ clf = RandomForestClassifier(max_depth=8,criterion="entropy",random_state=0,max_
 clf.fit(X,y)
 
 ##### Classifier was saved a pickle file for future use #####
+filename = 'finalized_model.sav'
+pickle.dump(clf, open(filename, 'wb'))
