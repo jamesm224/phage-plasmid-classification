8eb252a875ecfe98e55827e4181993194ae825d7
James Mullet <86495895+jamesm224@users.noreply.github.com>
2025-03-13 09:23:08 -0400

    Update making_gffs.sh

--- bin/making_gffs.sh
+++ bin/making_gffs.sh
@@ -5,7 +5,11 @@
 #SBATCH -N 1 
 #SBATCH -n 32
 
+# Loop through all nucleotide fasta in directory #
+# Ensure you have prokka installed to run this command - it uses the gff file prokka generates #
+# Alternatively to this script you can just run prokka yourself too! #
 samples=`ls *.fasta | awk '{split($_,x,".fa"); print x[1]}' | sort | uniq`
+
 for sample in $samples
 do
 mkdir ${sample}.gff.o
@@ -13,10 +17,9 @@ mkdir ${sample}.gff.o
 split -l 2 ${sample}.fasta
 Xs=x*
 for X in ${Xs}; do cp "$X" "${sample}.gff.o/$(head -1 "$X" | sed 's/>//g' | sed 's/ //g')"; done
-#mv ${sample}_ctg* ${sample}.o
+
 cd ${sample}.gff.o
-#psamples=`ls *_ctg | awk '{split($_,x,".summary.csv"); print x[1]}' | sort | uniq`
-#psamples=*_ctg* 
+
 psamples=*
 for psample in $psamples; do prokka --metagenome --mincontiglen 1500 --prefix ${psample}.prokka --metagenome --fast --cpus 32 ${psample}; done
 mkdir clusters
