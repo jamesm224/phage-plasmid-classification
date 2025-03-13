012ee1799fc2b3e1ae59ea3748e2002105129f26
James Mullet <86495895+jamesm224@users.noreply.github.com>
2025-03-13 09:21:46 -0400

    Update padloc_analysis.sh

--- bin/padloc_analysis.sh
+++ bin/padloc_analysis.sh
@@ -5,16 +5,21 @@
 #SBATCH -A prudenlab
 #SBATCH -N 1 
 #SBATCH -n 128
+
+##### Define Protein Files #####
 #proteins='/work/cascades/clb21565/WW_Analysis/hsp/MGE_analysis/context_analysis/reassembly_workdir/clinkers/myxos.txt.fasta'
 #proteins='/work/cascades/clb21565/WW_Analysis/hsp/MGE_analysis/context_analysis/reassembly_workdir/clinkers/annotationdb.faa'
+
+##### List test directory #####
 TESTDIR='/projects/ciwars/jamesm22/Final_Hybrid_Data/plasmid_padloc_data/'
 cd ${TESTDIR}
+
+# Note - you will need gff files - take a look at making_gffs.sh if you do not have those already #
+
+# Loop through samples #
 samples=`ls *.faa | awk '{split($_,x,".fa"); print x[1]}' | sort | uniq`
 for sample in $samples
 do
 padloc --faa ${sample}.faa --gff ${sample}.gff
-#prodigal -i ${sample}.fasta -o prodigal_out -a ${sample}.faa -p meta
-#padloc --faa /projects/ciwars/jamesm22/Final_Hybrid_Data/fastANI_Hybrids_Clustered/${sample}.faa --gff /projects/ciwars/jamesm22/Final_Hybrid_Data/fastANI_H
-ybrids_Clustered/${sample}.gff
-#echo ${sample}
+echo ${sample}
 done
